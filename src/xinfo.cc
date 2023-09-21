31aacd02b32b0ec3b4f86a30e71ed992787e8276
momokchung <kchung25@wustl.edu>
2023-09-21 15:22:56 -0500

    dcd support

--- src/xinfo.cc
+++ src/xinfo.cc
@@ -10,7 +10,7 @@ namespace tinker {
 static const char* getSHA1()
 {
    return         //
-      "b92eacc8"; // Mon Feb 20 00:29:57 2023 -0600
+      "4576f81c";
    // "556b58bf"; // Wed Feb 8 15:17:20 2023 -0600
    // "cc62211b"; // Sun Jan 29 15:31:49 2023 -0600
    // "cc8ef621"; // Mon Sep 19 16:51:16 2022 -0500
