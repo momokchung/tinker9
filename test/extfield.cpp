d8e275c232c3986b3a4e67e0757c2abbb69cdf91
Moses <kchung25@wustl.edu>
2025-09-03 10:55:34 -0500

    test NVE MD with external field and frequency

--- test/extfield.cpp
+++ test/extfield.cpp
@@ -1,4 +1,7 @@
 #include "ff/modamoeba.h"
+#include "md/integrator.h"
+#include "md/pq.h"
+#include <tinker/detail/extfld.hh>
 
 #include "test.h"
 #include "testrt.h"
@@ -412,6 +415,80 @@ TEST_CASE("External-Fields-VdwPchg", "[ff][extfield]")
    for (int i = 0; i < 3; ++i)
       for (int j = 0; j < 3; ++j)
          COMPARE_REALS(vir[i * 3 + j], ref_v[i][j], eps_v);
+
+   finish();
+   testEnd();
+}
+
+static const std::string externalFieldFreq = "exfld-freq  20.5\n";
+static const std::string integrator = "integrator verlet\n";
+
+TEST_CASE("External-Fields-Dynamic-Freq", "[ff][extfield]")
+{
+   int mask = calc::xyz | calc::vel | calc::mass | calc::energy | calc::grad | calc::md | calc::virial;
+
+   TestFile fx1(TINKER9_DIRSTR "/test/file/extfield/water4.xyz");
+   TestFile fd1(TINKER9_DIRSTR "/test/file/extfield/water4.dyn");
+   TestFile fk1(TINKER9_DIRSTR "/test/file/extfield/water4.key", "", externalField + externalFieldFreq + integrator);
+   TestFile fp1(TINKER9_DIRSTR "/test/file/commit_6fe8e913/water03.prm");
+   const char* xn = "water4.xyz";
+
+   // positions and velocities are saved as gradients in the text files
+   TestReference rvel(TINKER9_DIRSTR "/test/ref/extfield.5.p.txt");
+   TestReference rpos(TINKER9_DIRSTR "/test/ref/extfield.5.q.txt");
+   auto ref_pos = rpos.getGradient();
+   auto ref_vel = rvel.getGradient();
+   auto ref_v = rvel.getVirial();
+   const double eps_pos = 0.0001;
+   const double eps_vel = 0.001;
+   const double eps_vir = 0.001;
+
+   const char* argv[] = {"dummy", xn};
+   int argc = 2;
+   testBeginWithArgs(argc, argv);
+   testMdInit(0.0, 0.0);
+
+   rc_flag = mask;
+   initialize();
+
+   // NVE: zero initial velocities
+   const double dt_ps = 0.002;
+   const int nsteps = 10;
+   VerletIntegrator vvi(ThermostatEnum::NONE, BarostatEnum::NONE);
+   for (int i = 1; i <= nsteps; ++i) {
+      if (extfld::use_exfld and extfld::use_exfreq) {
+         double phs = sin(extfld::exfreq * (i-1) * dt_ps);
+         for (int j = 0; j < 3; j++) {
+            extfld::texfld[j] = phs * extfld::exfld[j];
+         }
+      }
+      vvi.dynamic(i, dt_ps);
+   }
+   std::vector<pos_prec> cx(n), cy(n), cz(n);
+   std::vector<vel_prec> px(n), py(n), pz(n);
+   darray::copyout(g::q0, n, cx.data(), xpos);
+   darray::copyout(g::q0, n, cy.data(), ypos);
+   darray::copyout(g::q0, n, cz.data(), zpos);
+   darray::copyout(g::q0, n, px.data(), vx);
+   darray::copyout(g::q0, n, py.data(), vy);
+   darray::copyout(g::q0, n, pz.data(), vz);
+   waitFor(g::q0);
+
+   for (int i = 0; i < n; ++i) {
+      COMPARE_REALS(cx[i], ref_pos[i][0], eps_pos);
+      COMPARE_REALS(cy[i], ref_pos[i][1], eps_pos);
+      COMPARE_REALS(cz[i], ref_pos[i][2], eps_pos);
+   }
+
+   for (int i = 0; i < n; ++i) {
+      COMPARE_REALS(px[i], ref_vel[i][0], eps_vel);
+      COMPARE_REALS(py[i], ref_vel[i][1], eps_vel);
+      COMPARE_REALS(pz[i], ref_vel[i][2], eps_vel);
+   }
+
+   for (int i = 0; i < 3; ++i)
+      for (int j = 0; j < 3; ++j)
+         COMPARE_REALS(vir[i * 3 + j], ref_v[i][j], eps_vir);
 
    finish();
    testEnd();
