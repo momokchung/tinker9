6f7d70f8e7c37a09258d3ab6cf3a4e2ecf81feff
Moses <kchung25@wustl.edu>
2025-09-11 18:07:34 -0500

    test for SAVE-ONLY keyword

--- test/tinkernist.cpp
+++ test/tinkernist.cpp
@@ -22,6 +22,8 @@ TEST_CASE("TinkerNIST-NODYN", "[ff][tinkerNIST]")
    TestFile fp1(TINKER9_DIRSTR "/test/file/commit_6fe8e913/amoeba09.prm");
    const char* xn = "water30.xyz";
    static const std::string fn = "water30";
+   fileExistsAndDelete(fn + ".arc");
+   fileExistsAndDelete(fn + ".dyn");
 
    const char* argv[] = {"dummy", xn};
    int argc = 2;
@@ -72,6 +74,8 @@ TEST_CASE("TinkerNIST-NOCOORD", "[ff][tinkerNIST]")
    TestFile fp1(TINKER9_DIRSTR "/test/file/commit_6fe8e913/amoeba09.prm");
    const char* xn = "water30.xyz";
    static const std::string fn = "water30";
+   fileExistsAndDelete(fn + ".arc");
+   fileExistsAndDelete(fn + ".dyn");
 
    const char* argv[] = {"dummy", xn};
    int argc = 2;
@@ -124,6 +128,11 @@ TEST_CASE("TinkerNIST-DCD", "[ff][tinkerNIST]")
    TestFile fp1(TINKER9_DIRSTR "/test/file/commit_6fe8e913/amoeba09.prm");
    const char* xn = "water30.xyz";
    static const std::string fn = "water30";
+   fileExistsAndDelete(fn + ".dcd");
+   fileExistsAndDelete(fn + ".dcdv");
+   fileExistsAndDelete(fn + ".dcduc");
+   fileExistsAndDelete(fn + ".dcdus");
+   fileExistsAndDelete(fn + ".dcdui");
 
    const char* argv[] = {"dummy", xn};
    int argc = 2;
@@ -182,6 +191,11 @@ TEST_CASE("TinkerNIST-SAVE", "[ff][tinkerNIST]")
    TestFile fp1(TINKER9_DIRSTR "/test/file/commit_6fe8e913/amoeba09.prm");
    const char* xn = "water30.xyz";
    static const std::string fn = "water30";
+   fileExistsAndDelete(fn + ".arc");
+   fileExistsAndDelete(fn + ".vel");
+   fileExistsAndDelete(fn + ".uchg");
+   fileExistsAndDelete(fn + ".ustc");
+   fileExistsAndDelete(fn + ".uind");
 
    auto arc_ref  = readAmoebaCoordinateFile(TINKER9_DIRSTR "/test/ref/tinkernist.arc");
    auto vel_ref  = readAmoebaCoordinateFile(TINKER9_DIRSTR "/test/ref/tinkernist.vel");
@@ -290,6 +304,78 @@ TEST_CASE("TinkerNIST-SAVE", "[ff][tinkerNIST]")
    REQUIRE(uchgExists == true);
    REQUIRE(ustcExists == true);
    REQUIRE(uindExists == true);
+
+   finish();
+   testEnd();
+}
+
+TEST_CASE("TinkerNIST-SAVE-ONLY", "[ff][tinkerNIST]")
+{
+   int mask = calc::xyz | calc::vel | calc::mass | calc::energy | calc::grad | calc::md | calc::virial;
+
+   TestFile fx1(TINKER9_DIRSTR "/test/file/tinkernist/water30.xyz");
+   TestFile fd1(TINKER9_DIRSTR "/test/file/tinkernist/water30.dyn");
+   static const std::string savekey = "SAVE-ONLY -1 99\nsave-ucharge\nsave-ustatic\nsave-uinduce\nsave-velocity\n";
+   TestFile fk1(TINKER9_DIRSTR "/test/file/tinkernist/water30.key", "", savekey + nodyn);
+   TestFile fp1(TINKER9_DIRSTR "/test/file/commit_6fe8e913/amoeba09.prm");
+   const char* xn = "water30.xyz";
+   static const std::string fn = "water30";
+   fileExistsAndDelete(fn + ".arc");
+   fileExistsAndDelete(fn + ".vel");
+   fileExistsAndDelete(fn + ".uchg");
+   fileExistsAndDelete(fn + ".ustc");
+   fileExistsAndDelete(fn + ".uind");
+
+   const char* argv[] = {"dummy", xn};
+   int argc = 2;
+   testBeginWithArgs(argc, argv);
+   testMdInit(0.0, 0.0);
+
+   rc_flag = mask;
+   initialize();
+
+   // NVE
+   const double dt_ps = 0.001;
+   const int nsteps = 2;
+   int old = inform::iwrite;
+   inform::iwrite = 1;
+   VerletIntegrator vvi(ThermostatEnum::NONE, BarostatEnum::NONE);
+   for (int istep = 1; istep <= nsteps; ++istep) {
+      vvi.dynamic(istep, dt_ps);
+
+      // mdstat
+      bool save = (istep % inform::iwrite == 0);
+      if (save || (istep % BOUNDS_EVERY_X_STEPS) == 0)
+         bounds();
+      if (save) {
+         T_prec temp;
+         kinetic(temp);
+         mdsaveAsync(istep, dt_ps);
+      }
+      mdrest(istep);
+   }
+   mdsaveSynchronize();
+   inform::iwrite = old;
+
+   auto arc_tst  = readAmoebaCoordinateFile(fn + ".arc");
+   auto vel_tst  = readAmoebaCoordinateFile(fn + ".vel");
+   auto uchg_tst = readAmoebaCoordinateFile(fn + ".uchg");
+   auto ustc_tst = readAmoebaCoordinateFile(fn + ".ustc");
+   auto uind_tst = readAmoebaCoordinateFile(fn + ".uind");
+
+   for (int istep = 0; istep < nsteps; ++istep) {
+      REQUIRE(arc_tst[0].size() == 99);
+      REQUIRE(vel_tst[0].size() == 99);
+      REQUIRE(uchg_tst[0].size() == 99);
+      REQUIRE(ustc_tst[0].size() == 99);
+      REQUIRE(uind_tst[0].size() == 99);
+   }
+
+   bool arcExists = fileExistsAndDelete(fn + ".arc");
+   bool velExists = fileExistsAndDelete(fn + ".vel");
+   bool uchgExists = fileExistsAndDelete(fn + ".uchg");
+   bool ustcExists = fileExistsAndDelete(fn + ".ustc");
+   bool uindExists = fileExistsAndDelete(fn + ".uind");
 
    finish();
    testEnd();
